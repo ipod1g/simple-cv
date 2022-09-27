1df5f2ace52f0db08442cbc61431a3a50ffaf7b0
ipod1g <kubonkwan99@gmail.com>
2022-09-28 00:39:01 +0800

    Add animated background & navigation bar

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,39 +2,69 @@ import React, { useRef } from "react";
 import About from "./About";
 import Experience from "./Experience";
 import "./Main.css";
+import Navbar from "./Navbar";
 import Projects from "./Projects";
 import ScrollBarIndicator from "./ScrollBarIndicator";
 import Skills from "./Skills";
 
 const Main = () => {
+   const aboutSection = useRef(null);
+   const experienceSection = useRef(null);
+   const projectsSection = useRef(null);
+   const skillsSection = useRef(null);
+
+   const scrollToSection = (elementRef) => {
+      window.scrollTo({
+         top: elementRef.current.offsetTop,
+         behavior: "smooth",
+      });
+   };
+
    return (
-      <div className="main-wrapper">
-         <section className="main-container">
-            <hr />
-            <About />
-            <hr />
-            <h3>Experience</h3>
-            <div className="xp-wrapper">
-               <ScrollBarIndicator height={740} origin={0} />
-               <ul className="xp-container">
-                  {/* <div className="xp-line"></div> */}
-                  <Experience></Experience>
-               </ul>
-            </div>
-            <hr />
-            <h3>Projects</h3>
-            <div className="proj-wrapper">
-               <ScrollBarIndicator height={901} origin={700} />
-               <ul>
-                  {/* <div className="proj-line"></div> */}
-                  <Projects></Projects>
-               </ul>
-            </div>
-            <hr />
-            <Skills />
-            <hr />
-         </section>
-      </div>
+      <>
+         <div className="main-wrapper">
+            <article className="main-container">
+               <hr />
+               <section className="about-section" ref={aboutSection}>
+                  <About />
+               </section>
+               <hr />
+               <section className="xp-section" ref={experienceSection}>
+                  <h3>Experience</h3>
+                  <div className="xp-wrapper">
+                     <ScrollBarIndicator height={740} origin={0} />
+                     <ul className="xp-container">
+                        {/* <div className="xp-line"></div> */}
+                        <Experience></Experience>
+                     </ul>
+                  </div>
+               </section>
+               <hr />
+               <section className="project-section" ref={projectsSection}>
+                  <h3>Projects</h3>
+                  <div className="proj-wrapper">
+                     <ScrollBarIndicator height={901} origin={700} />
+                     <ul>
+                        {/* <div className="proj-line"></div> */}
+                        <Projects></Projects>
+                     </ul>
+                  </div>
+               </section>
+               <hr />
+               <section className="skill-section" ref={skillsSection}>
+                  <Skills />
+               </section>
+               <hr />
+            </article>
+         </div>
+         <Navbar
+            scrollToSection={scrollToSection}
+            aboutSection={aboutSection}
+            experienceSection={experienceSection}
+            projectsSection={projectsSection}
+            skillsSection={skillsSection}
+         />
+      </>
    );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import "./Navbar.css";
 
-const Navbar = () => {
-   return <div>Navbar</div>;
+const Navbar = (props) => {
+   return (
+      <nav className="nav-bar">
+         <button
+            onClick={() => {
+               props.scrollToSection(props.aboutSection);
+            }}
+         >
+            Ab.
+         </button>
+         <button
+            onClick={() => {
+               props.scrollToSection(props.experienceSection);
+            }}
+         >
+            Xp.
+         </button>
+         <button
+            onClick={() => {
+               props.scrollToSection(props.projectsSection);
+            }}
+         >
+            Pj.
+         </button>
+         <button
+            onClick={() => {
+               props.scrollToSection(props.skillsSection);
+            }}
+         >
+            Sk.
+         </button>
+      </nav>
+   );
 };
 
 export default Navbar;
