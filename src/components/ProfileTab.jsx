1b7ea0a2f988343cd04cb48459a46c0717958b28
ipod1g <kubonkwan99@gmail.com>
2022-09-29 00:51:10 +0800

    Add selfie placeholder & update scrollbar style

--- src/components/ProfileTab.jsx
+++ src/components/ProfileTab.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./ProfileTab.css";
+import Selfie from "../assets/user1.png";
 
 const ProfileTab = () => {
    return (
       <div className="profile-tab-container">
          <div className="profile-tab">
-            <div>Prof Pic</div>
+            <div className="selfie-wrapper">
+               <img src={Selfie} alt="My selfie" height="80px" />
+            </div>
             <div>Lorem Ipsum Lorem Ipsum Lorem Ipsum</div>
             <div className="social">Lorem Ipsum</div>
             <div className="footer">Lorem Ipsum</div>
