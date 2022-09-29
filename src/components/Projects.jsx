38a49566c9b657e62dcb0306342c150ac2570b5a
ipod1g <kubonkwan99@gmail.com>
2022-09-30 00:29:04 +0800

    Add content & update styles - color theme

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,67 +12,66 @@ const Projects = (props) => {
          {/* divider */}
          <div className="proj-box-wrapper">
             <li data-aos="zoom-in-up" className="xp-box">
-               <a className="company" href="#">
-                  Good stuff
-               </a>
-               <p>
-                  LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT
-                  LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT
-               </p>
-               <ul>
-                  <li>Did this</li>
-                  <li>Did this</li>
-               </ul>
+               <div className="inner-container">
+                  <a className="company" href="#">
+                     CV website
+                  </a>
+                  <div className="role-date">
+                     <span>Solo </span>
+                     <span>Sep 22</span>
+                  </div>
+                  <br />
+                  <ul>
+                     <li>
+                        Created a resume-style interactive website using
+                        React.js.
+                     </li>
+                     <br />
+                     <li>
+                        Focused on clean and component factored coding style
+                     </li>
+                  </ul>
+               </div>
             </li>
          </div>
          {/* divider */}
          <div className="proj-box-wrapper">
             <li data-aos="zoom-in-up" className="xp-box">
-               <a className="company" href="#">
-                  Good stuff
-               </a>
-               <p>
-                  LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT
-                  LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT
-               </p>
-               <ul>
-                  <li>Did this</li>
-                  <li>Did this</li>
-               </ul>
+               <div className="inner-container">
+                  <a
+                     className="company"
+                     href="https://github.com/ipod1g/testers/tree/main/riot-login-clone"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                  >
+                     Riot Games Client Login Screen Clone
+                  </a>
+                  <div className="role-date">
+                     <span>Solo </span>
+                     <span>Aug - Sep 22</span>
+                  </div>
+                  <br />
+                  <ul>
+                     <li>
+                        Developed a functional registration and login
+                        authorization system connected to RDBMS (MySQL) database
+                        using React and Node.js.
+                     </li>
+                     <br />
+                     <li>
+                        Implemented responsive design and practised React Hook
+                        system
+                     </li>
+                     <br />
+                     <li>
+                        Optimized loading time by changing font loading method
+                        and refactoring code
+                     </li>
+                  </ul>
+               </div>
             </li>
          </div>
          {/* divider */}
-         <div className="proj-box-wrapper">
-            <li data-aos="zoom-in-up" className="xp-box">
-               <a className="company" href="#">
-                  Good stuff
-               </a>
-               <p>
-                  LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT
-                  LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT
-               </p>
-               <ul>
-                  <li>Did this</li>
-                  <li>Did this</li>
-               </ul>
-            </li>
-         </div>
-         {/* divider */}
-         <div className="proj-box-wrapper">
-            <li data-aos="zoom-in-up" className="xp-box">
-               <a className="company" href="#">
-                  Good stuff
-               </a>
-               <p>
-                  LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT
-                  LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT LONG TEXT
-               </p>
-               <ul>
-                  <li>Did this</li>
-                  <li>Did this</li>
-               </ul>
-            </li>
-         </div>
       </>
    );
 };
