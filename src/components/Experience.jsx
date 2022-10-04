778d2e1278b732269dc38767dd753f49333b55ad
ipod1g <kubonkwan99@gmail.com>
2022-10-05 01:21:10 +0800

    Replace aos functions with framer-motion

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,16 +1,11 @@
-import React, { useEffect, useRef } from "react";
-import Aos from "aos";
-import "aos/dist/aos.css";
+import React, { useRef } from "react";
+import { motion } from "framer-motion";
 import "./Experience.css";
 import ScrollBarIndicator from "./ScrollBarIndicator";
 
 const Experience = (props) => {
-   useEffect(() => {
-      Aos.init({ duration: 500 });
-   }, []);
-
-   const ref = useRef(null);
    // For scrolldimension
+   const ref = useRef(null);
 
    return (
       <>
@@ -18,81 +13,109 @@ const Experience = (props) => {
          <ul className="xp-container" ref={ref}>
             {/* divider */}
             <div className="xp-box-wrapper">
-               <li data-aos="zoom-in-up" className="xp-box">
-                  <h4>Summer Undergraduate Research Internship Program</h4>
-                  <div className="role-date">
-                     <span>Computational Biomolecules (hydrogel) </span>
-                     <span>May - July 22</span>
-                  </div>
-                  <br />
-                  <ul>
-                     <li>
-                        {" "}
-                        Simulated biophysical system with MARTINI force field
-                        and GROMACS to compare for compatibility of result under
-                        the supervision of Professor Wang Yi and awarded the
-                        best poster presentation prize amongst 18 other teams by
-                        the professors
-                     </li>
+               <motion.div
+                  initial={{ opacity: 0, scale: 0.5 }}
+                  whileInView={{
+                     opacity: 1,
+                     scale: 1,
+                  }}
+                  transition={{ ease: "easeOut", duration: 0.5 }}
+               >
+                  <li className="xp-box">
+                     <h4>Summer Undergraduate Research Internship Program</h4>
+                     <div className="role-date">
+                        <span>Computational Biomolecules (hydrogel) </span>
+                        <span>May - Jul 22</span>
+                     </div>
                      <br />
-                     <li>
-                        Utilized server-side simulation processing in
-                        LINUX-based OS and enhanced efficiency for the project
-                        by cooperating with a partner
-                     </li>
-                  </ul>
-               </li>
+                     <ul>
+                        <li>
+                           {" "}
+                           Simulated biophysical system with MARTINI force field
+                           and GROMACS to compare for compatibility of result
+                           under the supervision of Professor Wang Yi and
+                           awarded the best poster presentation prize amongst 18
+                           other teams by the professors
+                        </li>
+                        <br />
+                        <li>
+                           Utilized server-side simulation processing in
+                           LINUX-based OS and enhanced efficiency for the
+                           project by cooperating with a partner
+                        </li>
+                     </ul>
+                  </li>
+               </motion.div>
             </div>
             {/* divider */}
             <div className="xp-box-wrapper">
-               <li data-aos="zoom-in-up" className="xp-box">
-                  <h4>Republic of Korea Army (ROKA)</h4>
-                  <div className="role-date">
-                     <span>Mortar Squad Leader in the 28th Division</span>
-                     <span>June 20 – Dec 21</span>
-                  </div>
-                  <br />
-                  <ul>
-                     <li>
-                        Led, managed, and supervised a 12-person squad in 23
-                        field trainings under variating environments
-                     </li>
+               <motion.div
+                  initial={{ opacity: 0, scale: 0.5 }}
+                  whileInView={{
+                     opacity: 1,
+                     scale: 1,
+                  }}
+                  transition={{ ease: "easeOut", duration: 0.5 }}
+               >
+                  <li className="xp-box">
+                     <h4>Republic of Korea Army (ROKA)</h4>
+                     <div className="role-date">
+                        <span>Mortar Squad Leader in the 28th Division</span>
+                        <span>Jun 20 – Dec 21</span>
+                     </div>
                      <br />
-                     <li>
-                        Prepared and reviewed situation reports and operational
-                        briefs for officers
-                     </li>
-                  </ul>
-               </li>
+                     <ul>
+                        <li>
+                           Led, managed, and supervised a 12-person squad in 23
+                           field trainings under variating environments
+                        </li>
+                        <br />
+                        <li>
+                           Prepared and reviewed situation reports and
+                           operational briefs for officers
+                        </li>
+                     </ul>
+                  </li>
+               </motion.div>
             </div>
             {/* divider */}
             <div className="xp-box-wrapper">
-               <li data-aos="zoom-in-up" className="xp-box">
-                  <h4>
-                     United States - Republic of Korea Combined Command Post
-                     Training
-                  </h4>
-                  <div className="role-date">
-                     <span>
-                        English Linguist in the Combined Ground Component
-                        Command
-                     </span>
-                     <span>June – August 21</span>
-                  </div>
-                  <br />
-                  <ul>
-                     <li>
-                        Engaged in US-ROK training as a real-time English
-                        linguist for the department of Hospitality and Personnel
-                        under deployment
-                     </li>
+               <motion.div
+                  initial={{ opacity: 0, scale: 0.5 }}
+                  whileInView={{
+                     opacity: 1,
+                     scale: 1,
+                  }}
+                  transition={{ ease: "easeOut", duration: 0.5 }}
+               >
+                  <li className="xp-box">
+                     <h4>
+                        United States - Republic of Korea Combined Command Post
+                        Training
+                     </h4>
+                     <div className="role-date">
+                        <span>
+                           English Linguist in the Combined Ground Component
+                           Command
+                        </span>
+                        <span>Jun – Aug 21</span>
+                     </div>
                      <br />
-                     <li>
-                        Translated diplomatically and strategically in-depth
-                        documents for both parties in clear technical wordings
-                     </li>
-                  </ul>
-               </li>
+                     <ul>
+                        <li>
+                           Engaged in US-ROK training as a real-time English
+                           linguist for the department of Hospitality and
+                           Personnel under deployment
+                        </li>
+                        <br />
+                        <li>
+                           Translated diplomatically and strategically in-depth
+                           documents for both parties in clear technical
+                           wordings
+                        </li>
+                     </ul>
+                  </li>
+               </motion.div>
             </div>
          </ul>
       </>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,19 +1,14 @@
 import React, { useEffect, useRef } from "react";
-import Aos from "aos";
-import "aos/dist/aos.css";
-import "./Projects.css";
+import { motion } from "framer-motion";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { library } from "@fortawesome/fontawesome-svg-core";
 // import { faReact, faNode } from "@fortawesome/free-brands-svg-icons";
 import ScrollBarIndicator from "./ScrollBarIndicator";
+import "./Projects.css";
 
 // library.add(faReact, faNode);
 
 const Projects = (props) => {
-   useEffect(() => {
-      Aos.init({ duration: 500 });
-   }, []);
-
    const ref = useRef(null);
 
    return (
@@ -22,104 +17,132 @@ const Projects = (props) => {
          <ul className="proj-container" ref={ref}>
             {/* divider */}
             <div className="proj-box-wrapper">
-               <li data-aos="zoom-in-up" className="xp-box">
-                  <div className="inner-container">
-                     <a className="company" href="#">
-                        CV website
-                     </a>
-                     <div className="role-date">
-                        <span>Solo </span>
-                        <span>Sep 22</span>
+               <motion.div
+                  initial={{ opacity: 0, scale: 0.5 }}
+                  whileInView={{
+                     opacity: 1,
+                     scale: 1,
+                  }}
+                  transition={{ ease: "easeOut", duration: 0.5 }}
+               >
+                  <li className="xp-box">
+                     <div className="inner-container">
+                        <a className="company" href="#">
+                           CV website
+                        </a>
+                        <div className="role-date">
+                           <span>Solo </span>
+                           <span>Sep 22</span>
+                        </div>
+                        <br />
+                        <ul>
+                           <li>
+                              Created a glass-morphism themed interactive and
+                              responsive resume website using React and
+                              framer-motion
+                           </li>
+                           <br />
+                           <li>
+                              Focused on clean and reusable component factored
+                              coding style for convenient updates to contents
+                           </li>
+                           <br />
+                           <li>
+                              Optimized loading time by converting assets to
+                              appropriate file types like webp
+                           </li>
+                        </ul>
                      </div>
-                     <br />
-                     <ul>
-                        <li>
-                           Created a glass-morphism themed interactive and
-                           responsive resume website using React and
-                           framer-motion
-                        </li>
-                        <br />
-                        <li>
-                           Focused on clean and reusable component factored
-                           coding style for convenient updates to contents
-                        </li>
-                        <br />
-                        <li>
-                           Optimized loading time by converting assets to
-                           appropriate file types like webp
-                        </li>
-                     </ul>
-                  </div>
-               </li>
+                  </li>
+               </motion.div>
             </div>
             {/* divider */}
             <div className="proj-box-wrapper">
-               <li data-aos="zoom-in-up" className="xp-box">
-                  <div className="inner-container">
-                     <a
-                        className="company"
-                        href="https://github.com/ipod1g/testers/tree/main/riot-login-clone"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                     >
-                        Riot Games Client Login Screen Clone
-                     </a>
-                     <div className="role-date">
-                        <span>Solo </span>
-                        <span>Aug - Sep 22</span>
+               <motion.div
+                  initial={{ opacity: 0, scale: 0.5 }}
+                  whileInView={{
+                     opacity: 1,
+                     scale: 1,
+                  }}
+                  transition={{ ease: "easeOut", duration: 0.5 }}
+               >
+                  <li className="xp-box">
+                     <div className="inner-container">
+                        <a
+                           className="company"
+                           href="https://github.com/ipod1g/testers/tree/main/riot-login-clone"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
+                           Riot Games Client Login Screen Clone
+                        </a>
+                        <div className="role-date">
+                           <span>Solo </span>
+                           <span>Aug - Sep 22</span>
+                        </div>
+                        <br />
+                        <ul>
+                           <li>
+                              Developed a functional registration and login
+                              authorization system connected to RDBMS(MySQL)
+                              database using React and Node.js.
+                           </li>
+                           <br />
+                           <li>
+                              Implemented custom responsive design and utilized
+                              React Hooks API
+                           </li>
+                           <br />
+                           <li>
+                              Increased performance by changing font loading
+                              method and continuously refactoring code for
+                              better efficiency
+                           </li>
+                        </ul>
                      </div>
-                     <br />
-                     <ul>
-                        <li>
-                           Developed a functional registration and login
-                           authorization system connected to RDBMS(MySQL)
-                           database using React and Node.js.
-                        </li>
-                        <br />
-                        <li>
-                           Implemented custom responsive design and utilized
-                           React Hooks API
-                        </li>
-                        <br />
-                        <li>
-                           Increased performance by changing font loading method
-                           and continuously refactoring code for better
-                           efficiency
-                        </li>
-                     </ul>
-                  </div>
-               </li>
+                  </li>
+               </motion.div>
             </div>
             {/* divider */}
             <div className="proj-box-wrapper">
-               <li data-aos="zoom-in-up" className="xp-box">
-                  <div className="inner-container">
-                     <a
-                        className="company"
-                        href="https://github.com/ipod1g/testers/tree/main/riot-login-clone"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                     >
-                        Portfolio Website v1
-                     </a>
-                     <div className="role-date">
-                        <span>Solo </span>
-                        <span>Aug 22</span>
-                     </div>
-                     <br />
-                     <ul>
-                        <li>
-                           Developed a simplistic interactive website with
-                           vanilla Javascript
-                        </li>
+               <motion.div
+                  initial={{ opacity: 0, scale: 0.5 }}
+                  whileInView={{
+                     opacity: 1,
+                     scale: 1,
+                  }}
+                  transition={{ ease: "easeOut", duration: 0.5 }}
+               >
+                  <li className="xp-box">
+                     <div className="inner-container">
+                        <a
+                           className="company"
+                           href="https://github.com/ipod1g/portfolio-1/"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
+                           Portfolio Website v1
+                        </a>
+                        <div className="role-date">
+                           <span>Solo </span>
+                           <span>Aug 22</span>
+                        </div>
                         <br />
-                        <li>
-                           Deployed in custom domain and improved crawlability
-                           by following semantic format for enhanced SEO
-                        </li>
-                     </ul>
-                  </div>
-               </li>
+                        <ul>
+                           <li>
+                              Developed a simplistic interactive website with
+                              vanilla Javascript
+                           </li>
+                           <br />
+                           <li>
+                              Deployed in custom domain and improved
+                              crawlability by following semantic format for
+                              enhanced SEO
+                           </li>
+                        </ul>
+                     </div>
+                  </li>
+               </motion.div>
             </div>
             {/* divider */}
          </ul>
