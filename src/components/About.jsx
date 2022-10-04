261cc7601701df2a53efcd3332cb357e584a29b5
ipod1g <kubonkwan99@gmail.com>
2022-10-05 01:24:51 +0800

    Relocate button & changed fontsize on mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-import { motion } from "framer-motion";
 
 const About = () => {
    return (
       <div className="about-container">
          <div className="about-header">
             <h3>About</h3>
-            <motion.button whileHover={{ scale: 1.1 }}>
-               <a href="./directory/yourfile.pdf" download="newfilename">
-                  Download CV
-               </a>
-            </motion.button>
          </div>
          <p>
             Hi, I'm an undergraduate in Physics at The Chinese University of
