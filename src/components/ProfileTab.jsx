8e3a54f9b7a53c3cabbcadd3193c69e31096c04b
ipod1g <kubonkwan99@gmail.com>
2022-10-06 00:59:59 +0800

    Add profiletab content & styling

--- src/components/ProfileTab.jsx
+++ src/components/ProfileTab.jsx
@@ -1,19 +1,115 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import {
+   faGithub,
+   faLinkedin,
+   faInstagram,
+   faReact,
+} from "@fortawesome/free-brands-svg-icons";
 import "./ProfileTab.css";
-import Selfie from "../assets/user1.png";
+import Selfie from "../assets/download.jpg";
+
+library.add(faReact, faGithub, faLinkedin, faInstagram, faEnvelope);
 
 const ProfileTab = () => {
    return (
-      <div className="profile-tab-container">
-         <div className="profile-tab">
-            <div className="selfie-wrapper">
-               <img src={Selfie} alt="My selfie" height="80px" />
+      <>
+         <aside className="profile-tab-container">
+            <div className="profile-tab">
+               <div className="selfie-wrapper">
+                  <img
+                     className="selfie"
+                     src={Selfie}
+                     alt="My selfie"
+                     height="100px"
+                     width="100px"
+                  />
+               </div>
+               <summary>
+                  BSc in Physics, CUHK <br />
+                  Frontend |&nbsp;
+                  <FontAwesomeIcon
+                     icon="fa-brands fa-react"
+                     color="rgb(12, 211, 218)"
+                  />
+                  &nbsp; React{" "}
+                  <p
+                     style={{
+                        fontStyle: "italic",
+                     }}
+                     className="intro"
+                  >
+                     Prospective mentality, a self-motivator, and a creative and
+                     persevering deep problem solver{" "}
+                  </p>
+               </summary>
+               <div className="origins">👨‍👩‍👦‍👦🇰🇷 🎒🇲🇾 🎓🇭🇰 </div>
+               <motion.button
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.99 }}
+               >
+                  <a href="../assets/CVSep.pdf" download="newfilename">
+                     Download CV
+                  </a>
+               </motion.button>
             </div>
-            <div>Lorem Ipsum Lorem Ipsum Lorem Ipsum</div>
-            <div className="social">Lorem Ipsum</div>
-            <div className="footer">Lorem Ipsum</div>
-         </div>
-      </div>
+         </aside>
+
+         <footer className="footer">
+            <div className="social">
+               <motion.a
+                  className="fa-icon"
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/ipod1g"
+                  title=""
+                  whileHover={{ scale: 1.15, color: "#c199e5e0" }}
+                  whileTap={{ scale: 1 }}
+               >
+                  <FontAwesomeIcon icon="fa-brands fa-github" />
+               </motion.a>
+               <motion.a
+                  className="fa-icon"
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://www.instagram.com/bono420/"
+                  title=""
+                  whileHover={{ scale: 1.15, color: "#c199e5e0" }}
+                  whileTap={{ scale: 1 }}
+               >
+                  <FontAwesomeIcon icon="fa-brands fa-instagram" />
+               </motion.a>
+               <motion.a
+                  className="fa-icon"
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://www.linkedin.com/in/bon-kwan-ku-340082245/"
+                  title=""
+                  whileHover={{ scale: 1.15, color: "#c199e5e0" }}
+                  whileTap={{ scale: 1 }}
+               >
+                  <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+               </motion.a>
+               <motion.a
+                  className="fa-icon"
+                  target="_blank"
+                  rel="noreferrer"
+                  href="mailto:kubonkwan99@gmail.com"
+                  title=""
+                  whileHover={{ scale: 1.15, color: "#c199e5e0" }}
+                  whileTap={{ scale: 1 }}
+               >
+                  <FontAwesomeIcon icon="fa-regular fa-envelope" />
+               </motion.a>
+            </div>
+            <div style={{ fontSize: "11px", marginTop: "4px" }}>
+               © 2022 Bono | All Rights Reserved.
+            </div>
+         </footer>
+      </>
    );
 };
 
