c84f8c08b67fe5297ff795ee59f91518b6d1bc22
ipod1g <kubonkwan99@gmail.com>
2022-10-12 01:51:45 +0800

    Refactor code to separate content & remove repeat

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,134 +3,94 @@ import { motion } from "framer-motion";
 import "./Experience.css";
 import ScrollBarIndicator from "./ScrollBarIndicator";
 
-const Experience = () => {
+const ExperienceBox = ({ work, role, date, desc1, desc2, desc3 }) => {
+   return (
+      <>
+         <div className="xp-box-wrapper">
+            <motion.div
+               initial={{ opacity: 0, scale: 0.5 }}
+               whileInView={{
+                  opacity: 1,
+                  scale: 1,
+               }}
+               transition={{ ease: "easeOut", duration: 0.5 }}
+               viewport={{ once: true }}
+            >
+               <li className="content-box">
+                  <h4>{work}</h4>
+                  <div className="role-date">
+                     <span className="role">{role}</span>
+                     <span>{date}</span>
+                  </div>
+                  <hr />
+                  <ul>
+                     <li>{desc1}</li>
+                     {desc2 ? (
+                        <>
+                           <br />
+                           <li>{desc2}</li>
+                        </>
+                     ) : null}
+                     {desc3 ? (
+                        <>
+                           <br />
+                           <li>{desc3}</li>
+                        </>
+                     ) : null}
+                  </ul>
+               </li>
+            </motion.div>
+         </div>
+      </>
+   );
+};
+
+const worksArray = [
+   /* WORK TITLE, ROLE, DATE, DESC1, DESC2, DESC3 */
+   [
+      "Summer Undergraduate Research Internship Program",
+      "Computational Biomolecules (hydrogel)",
+      "May - Jul 22",
+      "Simulated biophysical system with MARTINI force field and GROMACS to compare for compatibility of result under the supervision of Professor Wang Yi",
+      "Awarded the best poster presentation prize amongst 18 other teams by the professors",
+      "Utilized server-side simulation processing inLINUX-based OS and enhanced efficiency for the project by cooperating with a partner",
+   ],
+   [
+      "Republic of Korea Army (ROKA)",
+      "Mortar Squad Leader in the 28th Division",
+      "Jun 20 – Dec 21",
+      "Led, managed, and supervised a 12-person squad in 23 field trainings under variating environments",
+      "Prepared and reviewed situation reports and operational briefs for officers",
+   ],
+   [
+      "United States - Republic of Korea Combined Command Post Training",
+      "English Linguist in the Combined Ground Component Command",
+      "Jun – Aug 21",
+      "Engaged in US-ROK training as a real-time English linguist for the department of Hospitality and Personnel under deployment",
+      "Translated diplomatically and strategically in-depth documents for both parties in clear technical wordings",
+   ],
+];
+
+export default function Experience() {
    // For scrolldimension
    const xpContainerRef = useRef(null);
 
    return (
       <>
          <ul className="xp-container" ref={xpContainerRef}>
-            {/* divider */}
-            <div className="xp-box-wrapper">
-               <motion.div
-                  initial={{ opacity: 0, scale: 0.5 }}
-                  whileInView={{
-                     opacity: 1,
-                     scale: 1,
-                  }}
-                  transition={{ ease: "easeOut", duration: 0.5 }}
-                  viewport={{ once: true }}
-               >
-                  <li className="content-box">
-                     <h4>Summer Undergraduate Research Internship Program </h4>
-                     <div className="role-date">
-                        <span className="role">
-                           Computational Biomolecules (hydrogel){" "}
-                        </span>
-                        <span>May - Jul 22</span>
-                     </div>
-                     <hr />
-                     <ul>
-                        <li>
-                           {" "}
-                           Simulated biophysical system with MARTINI force field
-                           and GROMACS to compare for compatibility of result
-                           under the supervision of Professor Wang Yi
-                        </li>
-                        <br />
-                        <li>
-                           Awarded the best poster presentation prize amongst 18
-                           other teams by the professors
-                        </li>
-                        <br />
-                        <li>
-                           Utilized server-side simulation processing in
-                           LINUX-based OS and enhanced efficiency for the
-                           project by cooperating with a partner
-                        </li>
-                     </ul>
-                  </li>
-               </motion.div>
-            </div>
-            {/* divider */}
-            <div className="xp-box-wrapper">
-               <motion.div
-                  initial={{ opacity: 0, scale: 0.5 }}
-                  whileInView={{
-                     opacity: 1,
-                     scale: 1,
-                  }}
-                  transition={{ ease: "easeOut", duration: 0.5 }}
-                  viewport={{ once: true }}
-               >
-                  <li className="content-box">
-                     <h4>Republic of Korea Army (ROKA)</h4>
-                     <div className="role-date">
-                        <span className="role">
-                           Mortar Squad Leader in the 28th Division
-                        </span>
-                        <span>Jun 20 – Dec 21</span>
-                     </div>
-                     <hr />
-                     <ul>
-                        <li>
-                           Led, managed, and supervised a 12-person squad in 23
-                           field trainings under variating environments
-                        </li>
-                        <br />
-                        <li>
-                           Prepared and reviewed situation reports and
-                           operational briefs for officers
-                        </li>
-                     </ul>
-                  </li>
-               </motion.div>
-            </div>
-            {/* divider */}
-            <div className="xp-box-wrapper">
-               <motion.div
-                  initial={{ opacity: 0, scale: 0.5 }}
-                  whileInView={{
-                     opacity: 1,
-                     scale: 1,
-                  }}
-                  transition={{ ease: "easeOut", duration: 0.5 }}
-                  viewport={{ once: true }}
-               >
-                  <li className="content-box">
-                     <h4>
-                        United States - Republic of Korea Combined Command Post
-                        Training
-                     </h4>
-                     <div className="role-date">
-                        <span className="role">
-                           English Linguist in the Combined Ground Component
-                           Command
-                        </span>
-                        <span>Jun – Aug 21</span>
-                     </div>
-                     <hr />
-                     <ul>
-                        <li>
-                           Engaged in US-ROK training as a real-time English
-                           linguist for the department of Hospitality and
-                           Personnel under deployment
-                        </li>
-                        <br />
-                        <li>
-                           Translated diplomatically and strategically in-depth
-                           documents for both parties in clear technical
-                           wordings
-                        </li>
-                     </ul>
-                  </li>
-               </motion.div>
-            </div>
+            {worksArray.map(([work, role, date, desc1, desc2, desc3]) => (
+               <ExperienceBox
+                  work={work}
+                  role={role}
+                  date={date}
+                  desc1={desc1}
+                  desc2={desc2}
+                  desc3={desc3}
+                  //    key={}
+               ></ExperienceBox>
+            ))}
          </ul>
          <ScrollBarIndicator ref={xpContainerRef} />
-         {/* divider */}
       </>
    );
-};
-
-export default Experience;
+}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,202 +1,149 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-// import { faReact, faNode } from "@fortawesome/free-brands-svg-icons";
 import ScrollBarIndicator from "./ScrollBarIndicator";
 import "./Projects.css";
 
 library.add(faGithub);
 
-const Projects = () => {
-   const projContainerRef = useRef(null);
-
+const ProjectsBox = ({
+   project,
+   projLink,
+   githubLink,
+   role,
+   date,
+   desc1,
+   desc2,
+   desc3,
+}) => {
    return (
       <>
-         <ul className="proj-container" ref={projContainerRef}>
-            {/* divider */}
-            <div className="proj-box-wrapper">
-               <motion.div
-                  initial={{ opacity: 0, scale: 0.5 }}
-                  whileInView={{
-                     opacity: 1,
-                     scale: 1,
-                  }}
-                  transition={{ ease: "easeOut", duration: 0.5 }}
-                  viewport={{ once: true }}
-               >
-                  <li className="content-box">
-                     <div className="content-box-inner-container">
-                        <div className="content-box-inner-buttons">
-                           <a className="company" href="#">
-                              CV Website
-                           </a>
-                           |
-                           <motion.a
-                              className="fa-icon"
-                              target="_blank"
-                              rel="noreferrer"
-                              href="https://github.com/ipod1g/simple-cv"
-                              title=""
-                              whileHover={{ scale: 1.15, color: "#c199e5e0" }}
-                              whileTap={{ scale: 1 }}
-                              style={{ marginTop: "1px" }}
-                           >
-                              <FontAwesomeIcon icon="fa-brands fa-github" />
-                           </motion.a>
-                        </div>
-                        <div className="role-date">
-                           <span className="role">Solo </span>
-                           <span>Sep 22</span>
-                        </div>
-                        <hr />
-                        <ul>
-                           <li>
-                              Created a glass-morphism themed interactive and
-                              responsive resume website using React and
-                              framer-motion
-                           </li>
-                           <br />
-                           <li>
-                              Focused on clean and reusable component factored
-                              coding style for convenient updates to contents
-                           </li>
-                           <br />
-                           <li>
-                              Optimized loading time by converting assets to
-                              appropriate file types like webp
-                           </li>
-                        </ul>
+         <div className="proj-box-wrapper">
+            <motion.div
+               initial={{ opacity: 0, scale: 0.5 }}
+               whileInView={{
+                  opacity: 1,
+                  scale: 1,
+               }}
+               transition={{ ease: "easeOut", duration: 0.5 }}
+               viewport={{ once: true }}
+            >
+               <li className="content-box">
+                  <div className="content-box-inner-container">
+                     <div className="content-box-inner-buttons">
+                        <a className="company" href={projLink}>
+                           {project}
+                        </a>
+                        |
+                        <motion.a
+                           className="fa-icon"
+                           target="_blank"
+                           rel="noreferrer"
+                           href={githubLink}
+                           title=""
+                           whileHover={{ scale: 1.15, color: "#c199e5e0" }}
+                           whileTap={{ scale: 1 }}
+                           style={{ marginTop: "1px" }}
+                        >
+                           <FontAwesomeIcon icon="fa-brands fa-github" />
+                        </motion.a>
                      </div>
-                  </li>
-               </motion.div>
-            </div>
-            {/* divider */}
-            <div className="proj-box-wrapper">
-               <motion.div
-                  initial={{ opacity: 0, scale: 0.5 }}
-                  whileInView={{
-                     opacity: 1,
-                     scale: 1,
-                  }}
-                  transition={{ ease: "easeOut", duration: 0.5 }}
-                  viewport={{ once: true }}
-               >
-                  <li className="content-box">
-                     <div className="content-box-inner-container">
-                        <div className="content-box-inner-buttons">
-                           <a
-                              className="company"
-                              href="https://testers4bono.netlify.app"
-                              target="_blank"
-                              rel="noopener noreferrer"
-                           >
-                              Riot Games Client Login Screen Clone
-                           </a>
-                           |
-                           <motion.a
-                              className="fa-icon"
-                              target="_blank"
-                              rel="noreferrer"
-                              href="https://github.com/ipod1g/testers/tree/main/riot-login-clone"
-                              title=""
-                              whileHover={{ scale: 1.15, color: "#c199e5e0" }}
-                              whileTap={{ scale: 1 }}
-                              style={{ marginTop: "1px" }}
-                           >
-                              <FontAwesomeIcon icon="fa-brands fa-github" />
-                           </motion.a>
-                        </div>
-                        <div className="role-date">
-                           <span className="role">Solo </span>
-                           <span>Aug - Sep 22</span>
-                        </div>
-                        <hr />
-                        <ul>
-                           <li>
-                              Developed a functional registration and login
-                              authorization system connected to RDBMS(MySQL)
-                              database using React and Node.js.
-                           </li>
-                           <br />
-                           <li>
-                              Implemented custom responsive design and utilized
-                              React Hooks API
-                           </li>
-                           <br />
-                           <li>
-                              Increased performance by changing font loading
-                              method and continuously refactoring code for
-                              better efficiency
-                           </li>
-                        </ul>
+                     <div className="role-date">
+                        <span className="role">{role}</span>
+                        <span>{date}</span>
                      </div>
-                  </li>
-               </motion.div>
-            </div>
-            {/* divider */}
-            <div className="proj-box-wrapper">
-               <motion.div
-                  initial={{ opacity: 0, scale: 0.5 }}
-                  whileInView={{
-                     opacity: 1,
-                     scale: 1,
-                  }}
-                  transition={{ ease: "easeOut", duration: 0.5 }}
-                  viewport={{ once: true }}
-               >
-                  <li className="content-box">
-                     <div className="content-box-inner-container">
-                        <div className="content-box-inner-buttons">
-                           <a
-                              className="company"
-                              href="https://bonoku.shop"
-                              target="_blank"
-                              rel="noopener noreferrer"
-                           >
-                              Portfolio Website v1
-                           </a>
-                           |
-                           <motion.a
-                              className="fa-icon"
-                              target="_blank"
-                              rel="noreferrer"
-                              href="https://github.com/ipod1g/portfolio-1/"
-                              title=""
-                              whileHover={{ scale: 1.15, color: "#c199e5e0" }}
-                              whileTap={{ scale: 1 }}
-                              style={{ marginTop: "1px" }}
-                           >
-                              <FontAwesomeIcon icon="fa-brands fa-github" />
-                           </motion.a>
-                        </div>
-                        <div className="role-date">
-                           <span className="role">Solo </span>
-                           <span>Aug 22</span>
-                        </div>
-                        <hr />
-                        <ul>
-                           <li>
-                              Developed a simplistic interactive website with
-                              vanilla Javascript
-                           </li>
-                           <br />
-                           <li>
-                              Deployed in custom domain and improved
-                              crawlability by using semantic elements for
-                              enhanced SEO
-                           </li>
-                        </ul>
-                     </div>
-                  </li>
-               </motion.div>
-            </div>
-            {/* divider */}
-         </ul>
-         <ScrollBarIndicator ref={projContainerRef} />
+                     <hr />
+                     <ul>
+                        <li>{desc1}</li>
+                        {desc2 ? (
+                           <>
+                              <br />
+                              <li>{desc2}</li>
+                           </>
+                        ) : null}
+                        {desc3 ? (
+                           <>
+                              <br />
+                              <li>{desc3}</li>
+                           </>
+                        ) : null}
+                     </ul>
+                  </div>
+               </li>
+            </motion.div>
+         </div>
       </>
    );
 };
 
-export default Projects;
+const projectsArray = [
+   /* PROJECT, PROJECT LINK, GITHUB LINK, ROLE, DATE, DESC1, DESC2, DESC3 */
+   [
+      "CV Website",
+      "#",
+      "https://github.com/ipod1g/simple-cv",
+      "Solo",
+      "Sep 22",
+      "Created a glass-morphism themed interactive and responsive resume website using React and framer-motion",
+      "Focused on clean and reusable component factored coding style for convenient updates to contents",
+      "Optimized loading time by converting assets to appropriate file types like webp",
+   ],
+   [
+      "Riot Games Client Login Screen Clone",
+      "https://testers4bono.netlify.app",
+      "https://github.com/ipod1g/testers/tree/main/riot-login-clone",
+      "Solo",
+      "Aug – Sep 22",
+      "Developed a functional registration and login authorization system connected to RDBMS(MySQL) database using React and Node.js",
+      "Implemented custom responsive design and utilized React Hooks API",
+      "Increased performance by changing font loading method and continuously refactoring code for better efficiency",
+   ],
+   [
+      "Portfolio Website v1",
+      "https://bonoku.shop",
+      "https://github.com/ipod1g/portfolio-1/",
+      "Solo",
+      "Aug 22",
+      "Developed a simplistic interactive website with vanilla Javascript",
+      "Deployed in custom domain and improved crawlability by using semantic elements for enhanced SEO",
+   ],
+];
+
+export default function Projects() {
+   // For scrolldimension
+   const projContainerRef = useRef(null);
+
+   return (
+      <>
+         <ul className="xp-container" ref={projContainerRef}>
+            {projectsArray.map(
+               ([
+                  project,
+                  projLink,
+                  githubLink,
+                  role,
+                  date,
+                  desc1,
+                  desc2,
+                  desc3,
+               ]) => (
+                  <ProjectsBox
+                     project={project}
+                     projLink={projLink}
+                     githubLink={githubLink}
+                     role={role}
+                     date={date}
+                     desc1={desc1}
+                     desc2={desc2}
+                     desc3={desc3}
+                     //    key={}
+                  ></ProjectsBox>
+               )
+            )}
+         </ul>
+         <ScrollBarIndicator ref={projContainerRef} />
+      </>
+   );
+}
