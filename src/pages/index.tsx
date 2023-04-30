c9ba1f195cb40a7900ecf438759ed22d6bade51c
ipod1g <kubonkwan99@gmail.com>
2023-04-30 17:58:53 +0800

    feat: add boilerplate for nested notion blocks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,36 @@
-import Image from 'next/image';
 import { Source_Sans_Pro } from 'next/font/google';
 import ProfileTab from '@/components/ProfileTab';
 import Main from '@/components/Main';
+import { getDatabase, parseDatabase } from '@/controllers/notion';
+import { DATABASE_ID } from '@/config';
 
 const source = Source_Sans_Pro({
   subsets: ['latin'],
   weight: ['400', '600', '700'],
 });
 
-export default function Home() {
+export default function Home({ notionData, rawData }) {
+  console.log(rawData);
+
   return (
     <div
       className={`${source.className} h-fit w-full flex lg:flex-row flex-col text-pale/90 tracking-[2px] leading-20`}
     >
       <ProfileTab />
-      <Main />
+      <Main notionData={notionData} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const database = await getDatabase(DATABASE_ID);
+  const parsedData = parseDatabase(database);
+
+  return {
+    props: {
+      rawData: database,
+      notionData: parsedData,
+    },
+    revalidate: 1,
+  };
+};
