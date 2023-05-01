cb72d89b0c3104b5d71ee72ce31a976df733404b
ipod1g <kubonkwan99@gmail.com>
2023-05-01 16:23:03 +0800

    feat: add suspense for notion api fetch

--- src/components/common/ContentBoxBullet.tsx
+++ src/components/common/ContentBoxBullet.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import Skeleton from './Skeleton';
 
 /** Not defining shape returns the default circle bullet */
 const ContentBoxBullet = (props: {
@@ -8,41 +9,49 @@ const ContentBoxBullet = (props: {
   switch (props.shape) {
     case 'diamond':
       return (
-        <div
-          className="before:bg-white before:absolute before:rotate-45 before:w-3
-      before:h-3 before:mt-7 before:z-50 before:left-0 before:content-['']"
-        >
-          {props.children}
-        </div>
+        <Suspense fallback={<Skeleton shape="diamond" />}>
+          <div
+            className="before:bg-white before:absolute before:rotate-45 before:w-3
+          before:h-3 before:mt-7 before:z-50 before:left-0 before:content-['']"
+          >
+            {props.children}
+          </div>
+        </Suspense>
       );
 
     case 'circle':
       return (
-        <div
-          className="before:bg-white before:absolute before:w-3
+        <Suspense fallback={<Skeleton shape="circle" />}>
+          <div
+            className="before:bg-white before:absolute before:w-3
       before:h-3 before:mt-7 before:z-50 before:left-0 before:content-[''] before:rounded-full"
-        >
-          {props.children}
-        </div>
+          >
+            {props.children}
+          </div>
+        </Suspense>
       );
 
     case 'triangle':
       return (
-        <div
-          className="before:bg-transparent before:absolute before:w-3
+        <Suspense fallback={<Skeleton shape="triangle" />}>
+          <div
+            className="before:bg-transparent before:absolute before:w-3
         before:h-3 before:mt-7 before:z-50 before:-left-[1px] before:content-[''] before:border-l-[8px] before:border-l-transparent before:border-t-[14px] before:border-t-white before:border-r-[8px] before:border-r-transparent"
-        >
-          {props.children}
-        </div>
+          >
+            {props.children}
+          </div>
+        </Suspense>
       );
     default:
       return (
-        <div
-          className="before:bg-white before:absolute before:w-3
+        <Suspense fallback={<Skeleton shape="circle" />}>
+          <div
+            className="before:bg-white before:absolute before:w-3
     before:h-3 before:mt-7 before:z-50 before:left-0 before:content-[''] before:rounded-full"
-        >
-          {props.children}
-        </div>
+          >
+            {props.children}
+          </div>
+        </Suspense>
       );
   }
 };
