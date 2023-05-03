01d9146cd5f3d02c47df4787997691a51e2b2ceb
ipod1g <kubonkwan99@gmail.com>
2023-05-04 01:47:46 +0800

    feat: setup api to notion

---
+++ src/pages/api/notion/databases/[id]/index.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { TOKEN, DATABASE_ID } from '@/config';
+import { getDatabase } from '@/controllers/notion';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  const { id } = req.query;
+
+  const dbId = id === 'cv_database' ? DATABASE_ID : id;
+
+  // const response = await fetch(
+  //   `https://api.notion.com/v1/databases/${dbId}/query`,
+  //   {
+  //     headers: {
+  //       'Notion-Version': '2022-06-28',
+  //       'Content-Type': 'application/json',
+  //       Authorization: `Bearer ${TOKEN}`,
+  //     },
+  //     // this took so long to resolve...
+  //     method: 'POST',
+  //     mode: 'cors',
+  //   }
+  // );
+
+  const response = getDatabase(dbId);
+
+  const data: any = await response;
+
+  res.status(200).json(data);
+}
