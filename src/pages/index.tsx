aad0c6c7e3971cc13adaa074260cc18a57457b46
ipod1g <kubonkwan99@gmail.com>
2023-05-05 22:59:56 +0800

    test: setup swr for ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,27 @@
 import NotionSection from '@/components/NotionSection';
+import { DATABASE_ID } from '@/config';
+import { getDatabase, parseDatabase } from '@/controllers/notionController';
+import { TNotionData } from '@/types/types';
+import { SWRConfig } from 'swr';
 
-export default function Home() {
-  return <NotionSection />;
+export const getStaticProps = async () => {
+  const database = await getDatabase(DATABASE_ID);
+  const parsedData = parseDatabase(database);
+
+  return {
+    props: {
+      fallback: {
+        notionDataArray: parsedData,
+      },
+    },
+    // revalidate: 1,
+  };
+};
+
+export default function Home({ fallback }: { fallback: TNotionData[] }) {
+  return (
+    <SWRConfig value={{ fallback }}>
+      <NotionSection />
+    </SWRConfig>
+  );
 }
-
-// export const getServerSideProps = async () => {
-//   const database = await getDatabase(DATABASE_ID);
-//   const parsedData = parseDatabase(database);
-
-//   return {
-//     props: {
-//       notionDataArray: parsedData,
-//     },
-//     // revalidate: 1,
-//   };
-// };
