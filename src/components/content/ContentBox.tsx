70fe54b1e0ac80a74df1ad445b48b675808b1b01
ipod1g <kubonkwan99@gmail.com>
2023-05-07 20:32:45 +0800

    chore: minor change to opacity

--- src/components/content/ContentBox.tsx
+++ src/components/content/ContentBox.tsx
@@ -20,7 +20,7 @@ const ContentBox = (props: { content: TNotionData }) => {
         }}
         transition={{ ease: 'easeOut', duration: 0.5 }}
         viewport={{ once: true }}
-        className="bg-white dark:bg-black/80 px-10 py-4 mb-20 ml-0 lg:ml-6 content-box"
+        className="bg-white dark:bg-black/70 px-10 py-4 mb-20 ml-0 lg:ml-6 content-box"
       >
         <div className="flex gap-1">
           <a
