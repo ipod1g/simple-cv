343f337068dc2b5d6f468242fd404e3f19b729e7
Bono <kubonkwan99@gmail.com>
2023-05-14 00:29:39 +0800

    ci: disable preview so it doesnt affect SEO

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
