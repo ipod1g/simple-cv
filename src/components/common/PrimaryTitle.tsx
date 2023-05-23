92fc6490da01a589c4dd7cd66f566c3b9b3033ba
Bono <kubonkwan99@gmail.com>
2023-05-24 02:04:39 +0800

    style: tweak to some CSS values

--- src/components/common/PrimaryTitle.tsx
+++ src/components/common/PrimaryTitle.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
+// fix wrap behavior
 const PrimaryTitle = ({ title }: { title: string }) => {
   return (
-    <div className="title-text font-semibold text-xl transition-all cursor-pointer max-w-[75%] dark:text-white duration-1000">
+    <div className="title-text font-semibold text-xl transition-all cursor-pointer pr-2 dark:text-white duration-1000">
       <span className="font-extralight text-xl opacity-70">|&nbsp;&nbsp;</span>
       {title}
     </div>
--- src/components/content/Main.tsx
+++ src/components/content/Main.tsx
@@ -8,7 +8,10 @@ const Main = (props: { children: React.ReactNode }) => {
   return (
     <>
       <main id="main-wrapper" className="h-fit w-full relative overflow-hidden">
-        <article id="main-container" className="h-fit w-full py-6">
+        <article
+          id="main-container"
+          className="h-fit w-full lg:w-[75vw] py-6 px-12"
+        >
           <section id="about-section">
             <About />
           </section>
