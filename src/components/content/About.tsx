01b154ca7fd81f333c05cf62d1f67a8f03e8d8a2
Bono <kubonkwan99@gmail.com>
2024-07-06 19:05:13 +0800

    feat: :speech_balloon: update about me description

--- src/components/content/About.tsx
+++ src/components/content/About.tsx
@@ -13,12 +13,12 @@ const About = () => {
       </div>
       <div className="leading-7 mb-[2px] pl-4 md:pl-16">
         <p>
-          Hi, I&apos;m a penultimate undergraduate in Physics at The Chinese
+          Hi, I&apos;m a graduate in Physics at The Chinese
           University of Hong Kong, CUHK.
         </p>
         <p>
-          Now I am a self-taught Frontend web developer passionate about
-          creating aesthetic and functional experiences! 😊
+          Now I&apos;m a self-taught Software Engineer passionate about
+          creating aesthetic and functional experiences on the web! 😊
         </p>
         <p>Check out my projects through the links!</p>
       </div>
